9278067f0c9e3a4c3a6ff5615f71bbccbad07cc4
Karen Javadyan <kajarenc@gmail.com>
2024-10-16 03:25:20 +0400

    add upload to s3

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import streamlit as st
+from utils import upload_to_s3
+from copy import deepcopy
+
+if "processed_files" not in st.session_state:
+    st.session_state.processed_files = dict()
 
 
-x = st.experimental_audio_input("SAY SOMETHING!!!")
+uploaded_audio_file = st.experimental_audio_input("SAY SOMETHING!!!")
 
-if x:
-    st.audio(x)
+
+if uploaded_audio_file is not None:
+    if uploaded_audio_file.file_id not in st.session_state.processed_files:
+        st.session_state.processed_files = dict()
+        st.session_state.processed_files[uploaded_audio_file.file_id] = {
+            "file": uploaded_audio_file,
+            "status": "pending",
+            "public_url": None,
+        }
+        public_url = upload_to_s3(deepcopy(uploaded_audio_file))
+        st.session_state.processed_files[uploaded_audio_file.file_id][
+            "public_url"
+        ] = public_url
+        st.session_state.processed_files[uploaded_audio_file.file_id][
+            "status"
+        ] = "uploaded_to_s3"
+
+
+st.write(st.session_state)
---
+++ utils.py
@@ -0,0 +1,26 @@
+import boto3
+import streamlit as st
+
+AWS_S3_BUCKET_NAME = st.secrets["AWS_S3_BUCKET_NAME"]
+AWS_REGION = st.secrets["AWS_REGION"]
+AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY"]
+AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+
+
+@st.cache_data
+def upload_to_s3(uploaded_file):
+    s3_client = boto3.client(
+        service_name="s3",
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+    )
+
+    s3_client.upload_fileobj(uploaded_file, AWS_S3_BUCKET_NAME, uploaded_file.name)
+
+    url = "https://s3-%s.amazonaws.com/%s/%s" % (
+        AWS_REGION,
+        AWS_S3_BUCKET_NAME,
+        uploaded_file.name,
+    )
+    return url
