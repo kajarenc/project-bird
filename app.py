58eac3613b32bbd239b5e150fdb074fbb2186ddf
Vincent Donato <vincent@streamlit.io>
2024-10-16 01:37:34 -0700

    Borrow code from SunoApi/SunoApi to get things working

--- app.py
+++ app.py
@@ -1,29 +1,69 @@
+import time
+
 import streamlit as st
-from utils import upload_to_s3
-from copy import deepcopy
 
-if "processed_files" not in st.session_state:
-    st.session_state.processed_files = dict()
+from cookie import new_suno_auth, start_keep_alive, get_random_token, suno_sqlite
+from utils import generate_music, suno_upload_audio, check_url_available
 
 
-uploaded_audio_file = st.experimental_audio_input("SAY SOMETHING!!!")
+@st.cache_resource
+def initialize():
+    """
+    IDENTITY = "razgriz.35@gmail.com"
+    SESSION = "sess_2nUbUioIE8cci4IcaLtfXVxyfdI"
+    COOKIE = "ajs_anonymous_id=b26c2460-7a5f-40f7-a582-d8d028e68d5e; _ga=GA1.1.561711694.1729030604; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8yblViU21LanJuOXlYc1oxRTZiRVpGQW43RHYiLCJyb3RhdGluZ190b2tlbiI6ImRkd2RydTZkbWZudnJnenI3cGdyZjV2Y3dtaWt2YzQ4cmRiYnFqMmkifQ.I_oDWyYEBVeMwQElLmJXAhD5rsFXxrHC0EPxKY9NDvdtjCRxzxWrCZhExHz3PdY5KePVSlpY85Bd-neZth4GpvObhapVvVwbGTctwC0Ve3vOchB8JjoXDgpXXcChmir1gswN18D7xjTrGQRJ_hBnsjv_1HfzBqXAw_w6bYv8lt7UdkLAsGW_vzFdi8QK6act4qShMK3Cssaw7i4TQ9GOeUWE1UwsXc7tn6pah_5-YoEk7fm3lTBFegFLcLAsWsnNRiprwQcdb-RDgoVWcU2OOMZ4T3YI7uP8uIl-NdFgwg01YxOldpy1GRs9naSsK5tOlnbXlEz68exYm_WzN3a1eg; __client_uat=1729030618; __client_uat_U9tcbTPE=1729030618; _cfuvid=duxLkYcnDIzVKTqd40InlnT4uo9Q48vqk.kTVWVVD4I-1729061150123-0.0.1.1-604800000; __stripe_mid=e8a2f2ed-7b63-43b7-a774-998ffe916a17b750bb; __stripe_sid=0da0dc47-a48a-4bc6-9571-dfa801228af4a59a16; _ga_7B0KEDD7XP=GS1.1.1729061151.2.1.1729062026.0.0.0; mp_26ced217328f4737497bd6ba6641ca1c_mixpanel=%7B%22distinct_id%22%3A%20%22d4e5009a-0077-4560-9868-6b8086c182dd%22%2C%22%24device_id%22%3A%20%22192924143e0334-088282ad763fa1-16525637-1d73c0-192924143e0334%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.com%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%2C%22%24user_id%22%3A%20%22d4e5009a-0077-4560-9868-6b8086c182dd%22%7D; __cf_bm=AmvxajhANOB.9Ks2SW2Uwxpa.dm0jC9V1a0CpeNPjC0-1729062077-1.0.1.1-pcg1ko.Ok8rhJ7k8nneaXbvaqQML0U4YL23iC1sWx6qL.Y_w0YOK8j1ToIPGFsZK49fZitNbS7IDnx8eq.i23g"
+    """
+
+    IDENTITY = "kajarenc@gmail.com"
+    SESSION = "sess_2nUeeIUXJ3oFyqDG6TVmoUUH1JH"
+    COOKIE = "_ga=GA1.1.585205625.1729032160; ajs_anonymous_id=438875d6-73c5-495f-ba79-9c6b3620ddb7; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8yblVlY0d5TGpudzNkZjRxWGoxbzlHNkhGcFUiLCJyb3RhdGluZ190b2tlbiI6Ijg2dDQwdzQ1NXU0Nmt2eWk3bnRmbDJiN3U2ZG83MWluOTgwM2k2ajMifQ.cXI-1vGA8RJkJwKGYdgyoD0D3nbS9fhgJaylnRunAxBevg2pgfs1XSlxsri6FvYPUz-NMz8dSstDgu5i77xztAVdp3D0QoC8vA1mb6NB-4dtR6T0CTUsX0xMWt8fBsIfYgihrN4zMf8sCbR7TRRjj9mt9bIUsbVOGTM-w1Cixnt0Y64CWot9M_CjqHmS-4qtmgGMWf4TmuRoB5RlmIMF6haRIiabNCXiUP0nKZAF10zFb3wZcWKDW6XtS7jsosOHX6suiMHkD3hkl7wgTi_G-MXVrNYzH_Rdf9cj01dLdSQLlHbciExtGcjBrAM7V81q5Z_XWu7vj-lbgyz58cybJw; __client_uat=1729032175; __client_uat_U9tcbTPE=1729032175; __stripe_mid=6c824372-68d9-4aec-909b-1d7dbb907910de1fd9; _cfuvid=NGkTvHpyWkbBjAErGoBGglfsVNhsC6cuvmAo4LvP0Dk-1729037517837-0.0.1.1-604800000; __cf_bm=gbV6zTAMrh6OoWzsjKuEtPlRj8ucEq7Xq.VMW5mq0rc-1729067137-1.0.1.1-kCxvVXoD6oFJ.la3fQkG6OLsE4_jzn8OOKQFNIk2.Kmblkayud53cYYFNe1oDPASN05PWrOgHgYVOXXPxoS.3w; _ga_7B0KEDD7XP=GS1.1.1729067137.2.0.1729067137.0.0.0; mp_26ced217328f4737497bd6ba6641ca1c_mixpanel=%7B%22distinct_id%22%3A%20%22ed21731f-e67d-447c-bcab-3f31fb679475%22%2C%22%24device_id%22%3A%20%22192925901f28e3-04dfa047351119-16525637-1d73c0-192925901f28e3%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.com%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%2C%22%24user_id%22%3A%20%22ed21731f-e67d-447c-bcab-3f31fb679475%22%7D"
+    existing_id = suno_sqlite.query_one("select id,identity,[session],cookie from session where identity =?", (IDENTITY,))
+    if existing_id:
+        existing_id = suno_sqlite.operate_one("update session set session=?, cookie=?, token=? where identity =?", (SESSION, COOKIE, IDENTITY,""))
+    else:
+        existing_id = suno_sqlite.operate_one("insert into session (identity,session,cookie,token) values(?,?,?,?)", (IDENTITY, SESSION, COOKIE,""))
+
+    if existing_id:
+        new_suno_auth(IDENTITY, SESSION, COOKIE)
+        start_keep_alive()
 
 
-if uploaded_audio_file is not None:
-    if uploaded_audio_file.file_id not in st.session_state.processed_files:
-        st.session_state.processed_files = dict()
-        st.session_state.processed_files[uploaded_audio_file.file_id] = {
-            "file": uploaded_audio_file,
-            "status": "pending",
-            "public_url": None,
-        }
-        public_url = upload_to_s3(deepcopy(uploaded_audio_file))
-        st.session_state.processed_files[uploaded_audio_file.file_id][
-            "public_url"
-        ] = public_url
-        st.session_state.processed_files[uploaded_audio_file.file_id][
-            "status"
-        ] = "uploaded_to_s3"
+initialize()
 
+if (audio := st.experimental_audio_input("Record some audio")):
+    upload_progress = st.progress(0, text="Uploading recording")
+    continue_clip_id = suno_upload_audio("my_audio.wav", audio.read(), get_random_token(), upload_progress)
 
-st.write(st.session_state)
+    data = {
+        "title": "My random title",  # TODO(vdonato): Title input
+        "tags": ",",
+        "prompt": "",
+        "mv": "chirp-v3-5",
+        "continue_at": None,
+        "continue_clip_id": continue_clip_id,
+        "generation_type":"TEXT",
+        "task": "extend",
+        "continued_aligned_prompt": None,
+        "negative_tags": "",
+        "infill_end_s": None,
+        "infill_start_s": None
+    }
+
+    resp = generate_music(data, get_random_token())
+    upload_progress.progress(90)
+
+    song_url1 = f"https://cdn1.suno.ai/{resp['clips'][0]['id']}.mp3"
+    song_url2 = f"https://cdn1.suno.ai/{resp['clips'][1]['id']}.mp3"
+
+    check_url_available(song_url1)
+    check_url_available(song_url2)
+
+    upload_progress.progress(99)
+
+    time.sleep(60)
+    upload_progress.progress(100)
+    upload_progress.empty()
+
+    st.audio(song_url1)
+    st.audio(song_url2)
+
--- utils.py
+++ utils.py
@@ -1,26 +1,159 @@
+# Taken from https://github.com/SunoApi/SunoApi/blob/main/utils.py
+
+import json
+import os
+import time
+import random
+import re
+import requests
+
 import boto3
 import streamlit as st
 
-AWS_S3_BUCKET_NAME = st.secrets["AWS_S3_BUCKET_NAME"]
-AWS_REGION = st.secrets["AWS_REGION"]
-AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY"]
-AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+S3_WEB_SITE_URL = os.getenv("S3_WEB_SITE_URL")
+S3_ACCESSKEY_ID = os.getenv("S3_ACCESSKEY_ID")
+S3_SECRETKEY_ID = os.getenv("S3_SECRETKEY_ID")
+
+COMMON_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    # "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Referer": "https://suno.com/",
+    "Origin": "https://suno.com",
+}
+
+BASE_URL = "https://studio-api.suno.ai"
+
+def fetch(url, headers=None, data=None, method="POST"):
+    if headers is None:
+        headers = {}
+    headers.update(COMMON_HEADERS)
+    if data is not None:
+        data = json.dumps(data)
+
+    try:
+        resp = None
+        requests.packages.urllib3.disable_warnings()
+        if method == "GET":
+            resp = requests.get(url=url, headers=headers, verify=False)
+        else:
+            resp = requests.post(url=url, headers=headers, data=data, verify=False)
+        if resp.status_code != 200:
+            print(resp.text)
+        if S3_WEB_SITE_URL is None or S3_WEB_SITE_URL == "https://cdn1.suno.ai":
+            result = resp.text
+        elif S3_WEB_SITE_URL is not None and "s3.bitiful.net" in S3_WEB_SITE_URL:
+            result = resp.text.replace('https://cdn1.suno.ai/', f'{S3_WEB_SITE_URL}/files/')
+        else:
+            result = resp.text.replace('https://cdn1.suno.ai/', 'https://res.sunoapi.net/files/')
+        result = result.replace('.png', '.png?fmt=webp&txt=SunoAPI&txt-size=0.35&txt-pos=0.5,*0.96&txt-alpha=0.30')
+        return json.loads(result)
+        # return resp.json()
+    except Exception as e:
+        return {"detail":str(e)}
+
+def get_page_feed(page, token):
+    headers = {"Authorization": f"Bearer {token}"}
+    # api_url = f"{BASE_URL}/api/feed/?ids={ids}"
+    api_url = f"{BASE_URL}/api/feed/?page={page}"
+    response = fetch(api_url, headers, method="GET")
+    return response
+
+def generate_music(data, token):
+    headers = {"Authorization": f"Bearer {token}"}
+    api_url = f"{BASE_URL}/api/generate/v2/"
+    response = fetch(api_url, headers, data)
+    return response
+
+def local_time():
+    return  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+def check_url_available(url, twice=False):
+    if S3_WEB_SITE_URL is None or S3_WEB_SITE_URL == "https://cdn1.suno.ai":
+        pass
+    elif S3_WEB_SITE_URL is not None and "s3.bitiful.net" in S3_WEB_SITE_URL:
+        url = url.replace(f'{S3_WEB_SITE_URL}/files/', 'https://cdn1.suno.ai/')
+    else:
+        url = url.replace(f'https://res.sunoapi.net/files/', 'https://cdn1.suno.ai/')
+    i = 0
+    while not twice and i < 10:
+        # 每间隔一秒钟检查一次url文件大小
+        file_size = get_file_size(url)
+        if file_size >= 1024*1024:
+            print(local_time() + f" ***check_url_available -> {url} 文件大小：{file_size} 大于1MB可访问到***\n")
+            break
+        i += 1
+        print(local_time() + f" ***check_url_available -> {url} 文件大小：{file_size} 小于1MB继续检查***\n")
+        time.sleep(2)
+    time.sleep(3)
+
+def get_file_size(url):
+    try:
+        requests.packages.urllib3.disable_warnings()
+        resp = requests.head(url, verify=False)
+        if resp.status_code == 200:
+            file_size = resp.headers.get('Content-Length')
+            if file_size:
+                return int(file_size)
+            # else:
+            #     return 0
+            print(local_time() + f" ***check_url_available -> {url} file_size -> {file_size} ***\n")
+        else:
+            print(local_time() + f" ***check_url_available -> {url} status_code -> {resp.status_code} ***\n")
+            return 0
+    except Exception as e:
+        print(local_time() + f" ***check_url_available -> {url} exception -> {str(e)} ***\n")
+        return 0
 
 
-@st.cache_data
-def upload_to_s3(uploaded_file):
-    s3_client = boto3.client(
-        service_name="s3",
-        region_name=AWS_REGION,
-        aws_access_key_id=AWS_ACCESS_KEY,
-        aws_secret_access_key=AWS_SECRET_KEY,
-    )
-
-    s3_client.upload_fileobj(uploaded_file, AWS_S3_BUCKET_NAME, uploaded_file.name)
-
-    url = "https://s3-%s.amazonaws.com/%s/%s" % (
-        AWS_REGION,
-        AWS_S3_BUCKET_NAME,
-        uploaded_file.name,
-    )
-    return url
+def suno_upload_audio(filename, bytes_data, token, my_bar):
+    try:
+        upload_url = f"{BASE_URL}/api/uploads/audio/"
+        data = {"extension": "mp3"}
+        resp = requests.post(upload_url, headers={"Authorization": f"Bearer {token}"}, data=json.dumps(data))
+        result = resp.json()
+        print(local_time() + f" ***suno_upload_audio -> {upload_url} upload request -> {result} ***\n")
+        my_bar.progress(20)
+        audio_id = result['id']
+        upload_url = result['url']
+        resp = requests.post(url=result['url'], data=result['fields'], files={"file": bytes_data}) 
+        if resp.status_code == 204:
+            print(local_time() + f" ***suno_upload_audio -> {upload_url} upload result -> {resp.status_code} ***\n")
+            data = {
+                "upload_type": "file_upload",
+                "upload_filename": filename
+            }
+            upload_url = f"{BASE_URL}/api/uploads/audio/{audio_id}/upload-finish/"
+            resp = requests.post(upload_url, headers={"Authorization": f"Bearer {token}"}, data=json.dumps(data))
+            result = resp.json()
+            print(local_time() + f" ***suno_upload_audio -> {upload_url} upload finish -> {result} ***\n")
+            my_bar.progress(40)
+            upload_url = f"{BASE_URL}/api/uploads/audio/{audio_id}/"
+            while True:
+                resp = requests.get(upload_url, headers={"Authorization": f"Bearer {token}"})
+                result = resp.json()
+                print(local_time() + f" ***suno_upload_audio -> {upload_url} upload status -> {result} ***\n")
+                if 'detail' in result and result['detail'] == "Unauthorized":
+                    pass
+                elif 'status' in result and result['status'] == "complete":
+                    break
+                elif 'status' in result and result['status'] == "error":
+                    return {"detail": result['error_message']}
+                else:
+                    time.sleep(5)
+            my_bar.progress(60)
+            upload_url = f"{BASE_URL}/api/uploads/audio/{audio_id}/initialize-clip/"
+            resp = requests.post(upload_url, headers={"Authorization": f"Bearer {token}"})
+            result = resp.json()
+            print(local_time() + f" ***suno_upload_audio -> {upload_url} initializa-clip -> {result} ***\n")
+            my_bar.progress(80)
+            return result['clip_id']
+        else:
+            print(local_time() + f" ***suno_upload_audio -> {upload_url} upload status_code -> {str(resp.status_code)} ***\n")
+            return {"detail": str(resp.status_code)}
+    except Exception as e:
+        print(local_time() + f" ***suno_upload_audio -> {upload_url} exception -> {str(e)} ***\n")
+        return {"detail": 'Unauthorized'}
